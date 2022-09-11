3bfa8fa9ff44e77b7b4ddd30a1b3e651814624bd
Hassan Abdulwahab <devhassan.org@gmail.com>
2022-09-12 00:10:43 +0100

    Create core model

--- core/model/src/main/java/com/devhassan/model/Movies.kt
+++ core/model/src/main/java/com/devhassan/model/Movies.kt
@@ -1,4 +1,31 @@
 package com.devhassan.model
 
-class Movies {
-}
+data class MoviesResponse(
+    val dates: Dates,
+    val page: Long,
+    val results: List<Movie>,
+    val totalPages: Long,
+    val totalResults: Long
+)
+
+data class Dates (
+    val maximum: String,
+    val minimum: String
+)
+
+data class Movie (
+    val adult: Boolean,
+    val backdropPath: String,
+    val genreIDS: List<Long>,
+    val id: Long,
+    val originalLanguage: String,
+    val originalTitle: String,
+    val overview: String,
+    val popularity: Double,
+    val posterPath: String,
+    val releaseDate: String,
+    val title: String,
+    val video: Boolean,
+    val voteAverage: Double,
+    val voteCount: Long
+)
