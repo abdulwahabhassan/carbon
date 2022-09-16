cc780e6f1fb235a05f6ae1b116ae90f46c4c1fba
Hassan Abdulwahab <devhassan.org@gmail.com>
2022-09-16 11:58:33 +0100

    Handle back press

--- app/src/main/java/com/devhassan/carbon/ui/CarbonApp.kt
+++ app/src/main/java/com/devhassan/carbon/ui/CarbonApp.kt
@@ -3,7 +3,9 @@ package com.devhassan.carbon.ui
 import android.annotation.SuppressLint
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
@@ -16,6 +18,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -50,6 +53,12 @@ fun CarbonApp(
                     ) {
                         if (appState.currentDestination?.route != MoviesDestination.route) {
                             Icon(
+                                modifier = Modifier
+                                    .clip(CircleShape)
+                                    .clickable {
+                                        appState.onBackClick()
+                                    }
+                                    .padding(6.dp),
                                 imageVector = Icons.Rounded.ArrowBack,
                                 contentDescription = "Back arrow",
                                 tint = purple
